91819f75359008be5760552da33051d1690f9b6e
Colin <colin.diesh@gmail.com>
2015-09-24 16:22:01 -0500

    Add assembly release

--- ncbi/aech/dl_asm.sh
+++ ncbi/aech/dl_asm.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-
-wget "ftp://ftp.ncbi.nlm.nih.gov/genomes/Acromyrmex_echinatior/CHR_Un/*.fa.gz"
-wget "ftp://ftp.ncbi.nlm.nih.gov/genomes/Acromyrmex_echinatior/protein/*.fsa.gz"
-wget "ftp://ftp.ncbi.nlm.nih.gov/genomes/Acromyrmex_echinatior/protein/*.fa.gz"
-wget "ftp://ftp.ncbi.nlm.nih.gov/genomes/Acromyrmex_echinatior/RNA/*.fsa.gz"
-wget "ftp://ftp.ncbi.nlm.nih.gov/genomes/Acromyrmex_echinatior/RNA/*.fa.gz"
+set -v
+wget --quiet "ftp://ftp.ncbi.nlm.nih.gov/genomes/Acromyrmex_echinatior/CHR_Un/*.fa.gz"
+wget --quiet "ftp://ftp.ncbi.nlm.nih.gov/genomes/Acromyrmex_echinatior/protein/*.fsa.gz"
+wget --quiet "ftp://ftp.ncbi.nlm.nih.gov/genomes/Acromyrmex_echinatior/protein/*.fa.gz"
+wget --quiet "ftp://ftp.ncbi.nlm.nih.gov/genomes/Acromyrmex_echinatior/RNA/*.fsa.gz"
+wget --quiet "ftp://ftp.ncbi.nlm.nih.gov/genomes/Acromyrmex_echinatior/RNA/*.fa.gz"
+wget --quiet "ftp://ftp.ncbi.nlm.nih.gov/genomes/Acromyrmex_echinatior/README_CURRENT_RELEASE"
