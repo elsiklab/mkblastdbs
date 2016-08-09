fc730fafcde12231fff0b9bdf47c37d97588d2e3
Colin <colin.diesh@gmail.com>
2016-08-09 11:29:08 -0500

    Move from genomes to refseq

--- ncbi_genomes/download_genomes.sh
+++ ncbi_genomes/download_genomes.sh
@@ -3,9 +3,9 @@
 genomes="Bombus_impatiens Wasmannia_auropunctata Solenopsis_invicta Pogonomyrmex_barbatus Nasonia_vitripennis Megachile_rotundata Linepithema_humile Harpegnathos_saltator Camponotus_floridanus Cerapachys_biroi Bombus_terrestris Bombus_impatiens Apis_mellifera Apis_florea Acromyrmex_echinatior Apis_dorsata Atta_cephalotes"
 
 for i in $genomes; do
-    echo $i;
-    wget --quiet -A "*.fa.gz" -X genomes/$i/ARCHIVE -r ftp://ftp.ncbi.nlm.nih.gov/genomes/$i/ 
-    wget --quiet -A "*.fsa.gz" -X genomes/$i/ARCHIVE -r ftp://ftp.ncbi.nlm.nih.gov/genomes/$i/ 
+    echo $i
+    ret=`curl -s -l ftp://ftp.ncbi.nlm.nih.gov/genomes/refseq/invertebrate/$i/latest_assembly_versions/`
+    wget --quiet -A "*genomic.fna.gz,*cds_from_genomic.fna.gz,*protein.faa.gz,*rna.fna.gz,*rna_from_genomic.fna.gz" ftp://ftp.ncbi.nlm.nih.gov/genomes/all/$ret/*
 done;
 
-find ftp.ncbi.nlm.nih.gov/ -type d -empty -delete
+
