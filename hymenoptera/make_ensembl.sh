b54e36c6a408b654d2e86ed9fcce1e541e9c2464
Colin <colin.diesh@gmail.com>
2016-08-17 09:51:33 -0500

    Reformat ensembl DBs

--- hymenoptera/make_ensembl.sh
+++ hymenoptera/make_ensembl.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+set -veu -o pipefail
+
+rm -rf ensembl
+SPECIES="apis_mellifera nasonia_vitripennis atta_cephalotes solenopsis_invicta bombus_impatiens"
+parallel --verbose -j 1 "wget --quiet -P ensembl ftp://ftp.ensemblgenomes.org/pub/release-32/metazoa/fasta/{}/cds/*.fa.gz" ::: $SPECIES
+parallel --verbose -j 1 "wget --quiet -P ensembl ftp://ftp.ensemblgenomes.org/pub/release-32/metazoa/fasta/{}/pep/*.fa.gz" ::: $SPECIES
 
 rm -rf ensembl_blast
-mkdir ensembl_blast/
+mkdir ensembl_blast
+pwd
 
-parallel -j 2 'gunzip -c {} | makeblastdb -in - -out ensembl_blast/{} -dbtype nucl -title "{.}.fa (Ensembl)" -parse_seqids' ::: ensembl/*cds*.fa.gz
-parallel -j 2 'gunzip -c {} | makeblastdb -in - -out ensembl_blast/{} -dbtype prot -title "{.}.fa (Ensembl)" -parse_seqids' ::: ensembl/*pep*.fa.gz
-
-
-
+parallel -j 1 'gunzip -c {} | makeblastdb -in - -out ensembl_blast/{} -dbtype nucl -title "{.}.fa (Ensembl)" -parse_seqids' ::: ensembl/*cds*
+parallel -j 1 'gunzip -c {} | makeblastdb -in - -out ensembl_blast/{} -dbtype prot -title "{.}.fa (Ensembl)" -parse_seqids' ::: ensembl/*pep*
